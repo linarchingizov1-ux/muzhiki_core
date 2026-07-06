8efc12fa4373b799713c562a3a305fa86490ba26
linar <linarchingizov1@gmail.com>
2026-07-06 05:39:36 -0700

    flutter appauth local

---
+++ package/flutter_appauth/ios/flutter_appauth/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "flutter_appauth",
+    platforms: [
+        .iOS("12.0")
+    ],
+    products: [
+        .library(name: "flutter-appauth", targets: ["flutter_appauth"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/openid/AppAuth-iOS", exact: "2.0.0"),
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "flutter_appauth",
+            dependencies: [
+                .product(name: "AppAuth", package: "AppAuth-iOS"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ],
+            cSettings: [
+                .headerSearchPath("include/flutter_appauth")
+            ]
+        )
+    ]
+)
