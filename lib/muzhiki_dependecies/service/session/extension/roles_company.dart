3acbb6d9cf96c61cddf8971f9c8ea497365f971d
linarchingizov1-ux <linarchingizov1@gmail.com>
2026-07-08 13:09:13 +0300

    asdasd

--- lib/muzhiki_dependecies/service/session/extension/roles_company.dart
+++ lib/muzhiki_dependecies/service/session/extension/roles_company.dart
@@ -1,4 +1,5 @@
 import 'package:muzhiki_core/muzhiki_dependecies/service/session/model/session_roles.dart';
+import 'package:talker/talker.dart';
 
 extension RolesCompany on List<RolesInfoModel> {
   List<RolesRankCompany> getCompaniesByRole(SessionRole? role) {
@@ -9,8 +10,24 @@ extension RolesCompany on List<RolesInfoModel> {
   }
 
   bool get accessAllowedInformator {
+    final talker = Talker();
     const allowedIds = {'1', '2', '3', '4', '10'};
 
-    return any((c) => allowedIds.contains(c.id));
+    talker.debug("--- ПРОВЕРКА ДОСТУПА ИНФОРМАТОРА ---");
+    talker.debug("Всего ролей у пользователя: $length");
+
+    for (final roleInfo in this) {
+      talker.debug(
+        "Проверяем роль: (id: ${roleInfo.id}, name: ${roleInfo.name})",
+      );
+
+      if (allowedIds.contains(roleInfo.id)) {
+        talker.debug("Доступ РАЗРЕШЕН для роли id: ${roleInfo.id}");
+        return true;
+      }
+    }
+
+    talker.debug("Доступ ЗАПРЕЩЕН: ни один id не совпал с $allowedIds");
+    return false;
   }
 }
