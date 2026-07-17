293f8bd552d94cff630b13ff22946f6dd531f78f
Tamerlan <t.dukov79@gmail.com>
2026-07-17 12:10:32 +0300

    fix обработка ошибок от сервера

--- lib/muzhiki_ui_kit/data/repository/bug_report_repository_impl.dart
+++ lib/muzhiki_ui_kit/data/repository/bug_report_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:muzhiki_core/muzhiki_dependecies/network/exception/network_map_error.dart';
 import 'package:muzhiki_core/muzhiki_ui_kit/config/report_problem_path.dart';
 import 'package:muzhiki_core/muzhiki_ui_kit/domain/repository/bug_report_repository.dart';
 
@@ -14,18 +15,22 @@ class BugReportRepositoryImpl implements BugReportRepository {
     required Map<String, dynamic> payload,
     String? screenshotPath,
   }) async {
-    final response = await _dio.post(
-      ReportProblemPath.bugReports,
-      data: FormData.fromMap({
-        'payload': MultipartFile.fromString(
-          jsonEncode(payload),
-          contentType: DioMediaType('application', 'json'),
-        ),
-        if (screenshotPath != null)
-          'screenshot': await MultipartFile.fromFile(screenshotPath),
-      }),
-    );
+    try {
+      final response = await _dio.post(
+        ReportProblemPath.bugReports,
+        data: FormData.fromMap({
+          'payload': MultipartFile.fromString(
+            jsonEncode(payload),
+            contentType: DioMediaType('application', 'json'),
+          ),
+          if (screenshotPath != null)
+            'screenshot': await MultipartFile.fromFile(screenshotPath),
+        }),
+      );
 
-    return response.data['success'] == true;
+      return response.data['success'] == true;
+    } catch (e, st) {
+      throw throw AppErrorMapper.I.map(e, st);
+    }
   }
 }
