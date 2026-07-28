83a47a107e9cb248e0a1b78594e7388f3503c0cf
linarchingizov1-ux <linarchingizov1@gmail.com>
2026-07-28 15:40:47 +0300

    add

--- lib/muzhiki_support/app/feature/presentation/main_view/support_view.dart
+++ lib/muzhiki_support/app/feature/presentation/main_view/support_view.dart
@@ -15,6 +15,7 @@ import 'package:muzhiki_core/muzhiki_support/app/feature/presentation/main_view/
 import 'package:muzhiki_core/muzhiki_support/app/feature/presentation/main_view/widgets/sliver_home_appbar_widget.dart';
 import 'package:muzhiki_core/muzhiki_support/app/feature/presentation/main_view/widgets/sliver_informator.dart';
 import 'package:muzhiki_core/muzhiki_support/app/feature/state/chat/chat_cubit.dart';
+import 'package:muzhiki_core/muzhiki_ui/muzhiki_ui.dart';
 
 class SupportView extends StatefulWidget {
   final String homeRoute, profileRoute;
@@ -101,26 +102,34 @@ class _SupportViewState extends State<SupportView> {
             child: BlocBuilder<ChatCubit, ChatState>(
               builder: (context, state) {
                 if (state.chatStatus == StateStatus.success) {
-                  return InkWell(
-                    onTap: () =>
-                        context.pushNamed(SupportRouteConstant.I.chatDraft),
-                    child: Container(
-                      height: 55.w,
-                      width: 55.w,
-                      padding: EdgeInsets.all(12.r),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: SupportColors.blood,
-                      ),
-                      child: Center(
-                        child: SvgPicture.asset(
-                          height: 15.w,
-                          width: 15.w,
-                          SupportAssets.I.svg.add,
-                        ),
-                      ),
-                    ),
+                  return MuzhikiUi.buttons.circle(
+                    size: 55,
+                    svgAsset: SupportAssets.I.svg.add,
+                    iconSize: 15,
+                    onTap: () {
+                      context.pushNamed(SupportRouteConstant.I.chatDraft);
+                    },
                   );
+                  // return InkWell(
+                  //   onTap: () =>
+                  //       context.pushNamed(SupportRouteConstant.I.chatDraft),
+                  //   child: Container(
+                  //     height: 55.w,
+                  //     width: 55.w,
+                  //     padding: EdgeInsets.all(12.r),
+                  //     decoration: BoxDecoration(
+                  //       shape: BoxShape.circle,
+                  //       color: SupportColors.blood,
+                  //     ),
+                  //     child: Center(
+                  //       child: SvgPicture.asset(
+                  //         height: 15.w,
+                  //         width: 15.w,
+                  //         SupportAssets.I.svg.add,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // );
                 } else {
                   return const SizedBox.shrink();
                 }
