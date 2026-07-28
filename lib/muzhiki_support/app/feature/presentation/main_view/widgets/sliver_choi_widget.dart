760247e07218f3a7830d6c947209b7f03f03d5e9
linarchingizov1-ux <linarchingizov1@gmail.com>
2026-07-28 16:56:53 +0300

    dasadsas

--- lib/muzhiki_support/app/feature/presentation/main_view/widgets/sliver_choi_widget.dart
+++ lib/muzhiki_support/app/feature/presentation/main_view/widgets/sliver_choi_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:muzhiki_core/muzhiki_support/app/data/websocket/extension/chat_extension.dart';
@@ -55,43 +54,25 @@ class SliverChoiWidget extends StatelessWidget {
                 scrolledUnderElevation: 0,
                 title: SizedBox(
                   height: 40.h,
-                  child:
-                      ListView.separated(
-                            scrollDirection: Axis.horizontal,
-                            separatorBuilder: (context, index) =>
-                                SizedBox(width: 5.w),
-                            itemCount: state.myChat!.channels.length,
-                            padding: EdgeInsets.zero,
-                            itemBuilder: (context, i) {
-                              final channel = state.myChat!.channels[i];
-                              return ChoiceWidgets(
-                                newMessage: state.myChat!.chats.unreadByChannel(
-                                  channel.id,
-                                ),
-                                onSelected: (v) =>
-                                    context.read<ChatCubit>().selecteChannel(
-                                      index: i,
-                                      channelId: channel.id,
-                                    ),
-                                isSelected: i == state.selectedChannels,
-                                label: channel.name,
-                              );
-                            },
-                          )
-                          .animate()
-                          .fadeIn(duration: 220.ms)
-                          .scaleXY(
-                            begin: 0.94,
-                            end: 1,
-                            curve: Curves.easeOutBack,
-                            duration: 320.ms,
-                          )
-                          .moveY(
-                            begin: 10,
-                            end: 0,
-                            duration: 320.ms,
-                            curve: Curves.easeOut,
-                          ),
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    separatorBuilder: (context, index) => SizedBox(width: 5.w),
+                    itemCount: state.myChat!.channels.length,
+                    padding: EdgeInsets.zero,
+                    itemBuilder: (context, i) {
+                      final channel = state.myChat!.channels[i];
+                      return ChoiceWidgets(
+                        newMessage: state.myChat!.chats.unreadByChannel(
+                          channel.id,
+                        ),
+                        onSelected: (v) => context
+                            .read<ChatCubit>()
+                            .selecteChannel(index: i, channelId: channel.id),
+                        isSelected: i == state.selectedChannels,
+                        label: channel.name,
+                      );
+                    },
+                  ),
                 ),
               );
             case StateStatus.userNotFound:
