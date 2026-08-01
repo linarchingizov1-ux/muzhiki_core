06a561d67c7622c231a9982846aabe1021995f78
linarchingizov1-ux <linarchingizov1@gmail.com>
2026-08-01 12:36:32 +0300

    dasdsaas

--- lib/muzhiki_ui/buttons/animated_button.dart
+++ lib/muzhiki_ui/buttons/animated_button.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-
-import 'package:flutter_bounceable/flutter_bounceable.dart';
+import 'package:go_router/go_router.dart';
 
 class AnimatedButton extends StatefulWidget {
   const AnimatedButton({
@@ -33,7 +32,15 @@ class AnimatedButton extends StatefulWidget {
   State<AnimatedButton> createState() => _AnimatedButtonState();
 }
 
-class _AnimatedButtonState extends State<AnimatedButton> {
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _scaleAnimation;
+  bool _isPopping = false;
+
+  GoRouter? _router;
+  VoidCallback? _routerListener;
+
   double _getScale() {
     if (widget.child != null) {
       return 1.05;
@@ -43,15 +50,128 @@ class _AnimatedButtonState extends State<AnimatedButton> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Bounceable(
-      scaleFactor: _getScale() - 1,
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
       duration: const Duration(milliseconds: 100),
-      reverseDuration: const Duration(milliseconds: 250),
+      reverseDuration: const Duration(milliseconds: 450),
+    );
 
-      onTap: widget.onTap,
+    _scaleAnimation = Tween<double>(begin: 1.0, end: _getScale()).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutBack,
+        reverseCurve: Curves.easeOutCubic,
+      ),
+    );
+  }
 
-      child: widget.child ?? _defaultButton(),
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final router = GoRouter.of(context);
+
+    if (identical(_router, router)) {
+      return;
+    }
+
+    _removeRouterListener();
+
+    _router = router;
+
+    _routerListener = () {
+      _resetPressState();
+    };
+
+    router.routerDelegate.addListener(_routerListener!);
+  }
+
+  @override
+  void dispose() {
+    _removeRouterListener();
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  void _removeRouterListener() {
+    final router = _router;
+    final listener = _routerListener;
+
+    if (router != null && listener != null) {
+      router.routerDelegate.removeListener(listener);
+    }
+
+    _router = null;
+    _routerListener = null;
+  }
+
+  void _resetPressState() {
+    if (!mounted) return;
+
+    _isPopping = false;
+
+    _controller
+      ..stop()
+      ..value = 0.0;
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    if (_isPopping) return;
+
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) async {
+    if (_isPopping) return;
+
+    _isPopping = true;
+
+    try {
+      final animation = _controller.forward();
+
+      await _controller.animateTo(
+        0.5,
+        duration: const Duration(milliseconds: 50),
+      );
+
+      if (mounted) {
+        widget.onTap();
+      }
+
+      await animation;
+
+      if (mounted) {
+        await _controller.reverse();
+      }
+    } finally {
+      _isPopping = false;
+    }
+  }
+
+  void _onTapCancel() {
+    if (_isPopping) return;
+
+    _controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: AnimatedBuilder(
+        animation: _scaleAnimation,
+        builder: (context, child) {
+          return Transform.scale(scale: _scaleAnimation.value, child: child);
+        },
+        child: widget.child ?? _defaultButton(),
+      ),
     );
   }
 
