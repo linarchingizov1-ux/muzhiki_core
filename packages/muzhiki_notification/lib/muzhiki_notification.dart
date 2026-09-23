7b303fa8820407bff4b6f255f3a5edc70fc6a24e
linarchingizov1-ux <linarchingizov1@gmail.com>
2026-09-23 09:45:31 +0300

    Изменения в части [muzhiki_notification] конкретно [muzhiki_notification.dart] в части export пакета [device_info_plus] чтобы не тянуть его в основной проект

--- packages/muzhiki_notification/lib/muzhiki_notification.dart
+++ packages/muzhiki_notification/lib/muzhiki_notification.dart
@@ -2,3 +2,4 @@ export 'config/notification_config.dart';
 export 'config/notification_storage.dart';
 export 'service/notification_push_service.dart';
 export 'widgets/notification_dialog.dart';
+export 'package:device_info_plus/device_info_plus.dart';
